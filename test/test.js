b426df621c7ec6f756bda09cf0706f457edcc986
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2014-09-12 17:28:02 +0900

    Add more tests to cover various cases

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 'use strict';
 
+var bufferEqual = require('buffer-equal');
 var File = require('vinyl');
 var fs = require('fs');
 var isWebP = require('is-webp');
@@ -21,6 +22,45 @@ test('optimize a PNG', function (t) {
 		stream.on('data', function (data) {
 			t.assert(data.contents.length < buf.length);
 			t.assert(isWebP(data.contents));
+		});
+
+		stream.end(file);
+	});
+});
+
+test('skip optimizing unsupported files', function (t) {
+	t.plan(2);
+
+	fs.readFile(path.join(__dirname, 'fixtures/test-unsupported.bmp'), function (err, buf) {
+		t.assert(!err);
+
+		var stream = webp();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('data', function (data) {
+			t.assert(bufferEqual(data.contents, buf));
+		});
+
+		stream.end(file);
+	});
+});
+
+test('throw error when an image is corrupt', function (t) {
+	t.plan(3);
+
+	fs.readFile(path.join(__dirname, 'fixtures/test-corrupt.jpg'), function (err, buf) {
+		t.assert(!err);
+
+		var stream = webp();
+		var file = new File({
+			contents: buf
+		});
+
+		stream.on('error', function (err) {
+			t.assert(err);
+			t.assert(/Corrupt JPEG data/.test(err.message));
 		});
 
 		stream.end(file);
