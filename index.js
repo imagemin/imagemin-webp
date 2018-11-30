7d803957e1dcc7ab453d8112b6a6d93fc4d88b3a
1000ch <shogo.sensui@gmail.com>
2018-11-30 21:46:11 +0900

    Require Node.js 6

--- index.js
+++ index.js
@@ -77,8 +77,8 @@ module.exports = opts => buf => {
 		input: buf,
 		bin: cwebp,
 		args
-	}).catch(err => {
-		err.message = err.stderr || err.message;
-		throw err;
+	}).catch(error => {
+		error.message = error.stderr || error.message;
+		throw error;
 	});
 };
