707aeb3ddf0268a57d617c7623956113f5f4df1e
Shogo Sensui <shogosensui@gmail.com>
2020-05-24 17:51:15 +0900

    Require Node.js 10 (#30)
    * Require Node.js 10
    
    * Use util.promisify

--- test.js
+++ test.js
@@ -1,14 +1,14 @@
-import fs from 'fs';
-import path from 'path';
-import isWebP from 'is-webp';
-import pify from 'pify';
-import test from 'ava';
-import imageminWebp from '.';
+const {promisify} = require('util');
+const fs = require('fs');
+const path = require('path');
+const isWebP = require('is-webp');
+const test = require('ava');
+const imageminWebp = require('.');
 
-const fsP = pify(fs);
+const readFile = promisify(fs.readFile);
 
 test('convert an image into a WebP', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures/test.png'));
+	const buf = await readFile(path.join(__dirname, 'fixtures/test.png'));
 	const data = await imageminWebp()(buf);
 
 	t.true(data.length < buf.length);
@@ -16,13 +16,13 @@ test('convert an image into a WebP', async t => {
 });
 
 test('skip optimizing unsupported files', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures/test-unsupported.bmp'));
+	const buf = await readFile(path.join(__dirname, 'fixtures/test-unsupported.bmp'));
 	const data = await imageminWebp()(buf);
 
 	t.deepEqual(data, buf);
 });
 
 test('throw error when an image is corrupt', async t => {
-	const buf = await fsP.readFile(path.join(__dirname, 'fixtures/test-corrupt.webp'));
-	await t.throws(imageminWebp()(buf), /BITSTREAM_ERROR/);
+	const buf = await readFile(path.join(__dirname, 'fixtures/test-corrupt.webp'));
+	await t.throwsAsync(() => imageminWebp()(buf), {message: /BITSTREAM_ERROR/});
 });
